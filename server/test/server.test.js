178320e33acea71026829f2e1cc8247db1778b08
Steve Martin San Jose <stevemartinsanjose@SMDS.local>
2019-01-04 13:17:44 +0800

    Add test cases for PATCH /todos route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -138,3 +138,41 @@ describe('DELETE /todos:id', () => {
       .end(done);
   });
 });
+
+describe('PATCH /todos:id', () => {
+  it('should update the todo', (done) => {
+    const hexId = todos[0]._id.toHexString();
+    const text = 'Updated First Todo test';
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text,
+        completed: true
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done)
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    const hexId = todos[1]._id.toHexString();
+    const text = 'Update Second Todo test';
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text,
+        completed: false
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
+      .end(done);
+  });
+});
