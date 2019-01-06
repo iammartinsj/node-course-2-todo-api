92214913ec899306743834c06f445734adfdbf0c
Steve Martin San Jose <stevemartinsanjose@SMDS.local>
2019-01-06 17:28:06 +0800

    Add text case DELETE /users/me/token

--- server/server.js
+++ server/server.js
@@ -135,7 +135,7 @@ app.post('/users/login', (req, res) => {
 app.delete('/users/me/token', authenticate, (req, res) => {
   req.user.removeToken(req.token).then(() => {
     res.status(200).send()
-  },(e) => {
+  }).catch((e) => {
     res.status(400).send(e);
   });
 });
